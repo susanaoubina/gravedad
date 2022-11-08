e4fc919a4f29cfc32d8e4530031c750f8f4dd6f2
susanaoubina <susana.oubinafalcon@gmail.com>
2022-11-08 14:23:37 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ basic.forever(function () {
     ax = input.acceleration(Dimension.X)
     ay = input.acceleration(Dimension.Y)
     az = input.acceleration(Dimension.Z)
-    a = Math.sqrt(input.acceleration(Dimension.X) * input.acceleration(Dimension.X) + (input.acceleration(Dimension.Y) * input.acceleration(Dimension.Y) + input.acceleration(Dimension.Z) * input.acceleration(Dimension.Z)))
+    a = Math.sqrt(ax * ax + (ay * ay + az * az))
     basic.showNumber(a / 1023 * 10)
     basic.pause(500)
 })
